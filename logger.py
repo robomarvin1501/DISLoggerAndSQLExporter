c1daef2d227b05a0bb5162b3f9e1649ea01194ae
8Oldr <8Oldr@protonmail.com>
2022-03-02 13:35:45 +0200

    Create the pcapng, and json files on rotation

---
+++ logger.py
@@ -0,0 +1,42 @@
+import subprocess
+import os
+import sys
+
+logger_processing_dir = r"C:\Users\gidonr\LoggerFiles"
+folders = os.listdir(logger_processing_dir)
+if "jsons" not in folders:
+    os.mkdir(f"{logger_processing_dir}/jsons")
+if "pcapngs" not in folders:
+    os.mkdir(f"{logger_processing_dir}/pcapngs")
+
+subprocess.run(["powershell", "Start-Process", "-FilePath", r"'C:\Program Files\Wireshark\tshark.exe'", "-ArgumentList",
+                rf"""'-i Ethernet -f "udp src port 3000 and udp dst port 3000" -b duration:2 -b files:5 -w {logger_processing_dir}\pcapngs\mult_output.pcapng'""",
+                "-PassThru"])
+
+
+def convert_to_json(target_input_file, target_output_file, logger_processing_dir=logger_processing_dir):
+    convert_to_json_instruction = ["powershell", "Start-Process",
+                                   "-FilePath", r"'C:\Program Files\Wireshark\tshark.exe'",
+                                   "-ArgumentList",
+                                   rf"'-r {logger_processing_dir}\pcapngs\{target_input_file} -Tjson'",
+                                   "-RedirectStandardOutput", f"{logger_processing_dir}\jsons\{target_output_file}",
+                                   "-NoNewWindow"]
+    subprocess.run(convert_to_json_instruction)
+
+
+processed_pcap_cache = set()
+while True:
+    try:
+        every_cap = os.listdir(fr"{logger_processing_dir}\pcapngs")
+        unprocessed_captures = [cap for cap in every_cap if cap not in processed_pcap_cache]
+        for cap_file in unprocessed_captures:
+            if cap_file != every_cap[-1]:
+                convert_to_json(cap_file, f"{cap_file[:-7]}.json", logger_processing_dir=logger_processing_dir)
+                processed_pcap_cache.add(cap_file)
+
+                jsons = os.listdir(f"{logger_processing_dir}/jsons")
+                if len(jsons) > 5:
+                    subprocess.run(["del", fr"{logger_processing_dir}\jsons\{min(jsons)}"], shell=True)
+    except KeyboardInterrupt:
+        print("Program ended")
+        sys.exit()
