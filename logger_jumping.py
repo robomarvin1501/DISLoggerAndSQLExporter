a82fbb03402f2dcf18e9a8f7091df9447cd8a25d
8Oldr <8Oldr@protonmail.com>
2023-04-19 12:37:56 +0300

    Documentation for the PlaybackLoggerfileManager

--- logger_jumping.py
+++ logger_jumping.py
@@ -87,10 +87,28 @@ def sender(pdu_queue: multiprocessing.connection.PipeConnection,
 
 
 class PlaybackLoggerFileManager:
+    """
+    This class handles playing back the logger file.
+    It holds the sender function, and sends it information to be transmitted to the network, handles moving the location
+    in the loggerfile, and everything required to make these things work.
+    It isn't really handled directly by you, but more interfaced through other provided classes.
+    """
+
     def __init__(self, loggername: str, pdu_queue: multiprocessing.connection.Connection,
                  message_queue: multiprocessing.connection.Connection,
                  returning_information_queue: multiprocessing.SimpleQueue,
                  exercise_id: int = 20):
+        """
+        :param loggername:                  str                                     : Absolute path to the logger file
+        :param pdu_queue:                   multiprocessing.connection.Connection   : Pipe that holds the PDUs that are
+                                                                                        to be sent
+        :param message_queue:               multiprocessing.connection.Connection   : Pipe that holds messages that are
+                                                                                        to be sent
+        :param returning_information_queue: multiprocessing.SimpleQueue             : SimpleQueue through which
+                                                                                        information returns from the
+                                                                                        sender thread
+        :param exercise_id:                 int
+        """
         self.unprocessed_pdus: list[bytes] = []
         self.logger_pdus: list[tuple[bytes, float]] = []
         self.position_pointer = 0
@@ -109,6 +127,7 @@ class PlaybackLoggerFileManager:
         self._maximum_time = 0
         self.stop_time = datetime.datetime.now().timestamp()
 
+        # A cache of the positions of all entities, so that when paused, they can still be seen.
         self.state_cache: dict[bytes, bytes] = dict()
         self.paused = False
 
@@ -121,7 +140,13 @@ class PlaybackLoggerFileManager:
 
         self.load_logger(loggername)
 
-    def _closer_time_then_neighbours(self, n, target_time):
+    def _closer_time_then_neighbours(self, n: int, target_time: float) -> int:
+        """
+        For a pdu of position n in self.logger_pdus, this checks if the target time is closer to pdu n - 1, n, or n + 1
+        :param n:               int     : Position in self.logger_pdus
+        :param target_time:     float   : Target time in seconds from the start of the logger file
+        :return: int
+        """
         # 0: n is the closest
         # 1: n - 1 is closer
         # 2: n + 1 is closer
@@ -147,6 +172,14 @@ class PlaybackLoggerFileManager:
             return 0
 
     def _binary_search_for_time(self, time: float, high: int = 0, low: int = 0) -> int:
+        """
+        Executes a binary search over self.logger_pdus to search for the pdu that is closest to the desired time.
+        Returns the position of the closest pdu
+        :param time: float  : Time in seconds since the start of the logger file
+        :param high: int    : Upper bound of self.logger_pdus to be searched
+        :param low:  int    : Lower bound of self.logger_pdus to be searched
+        :return: int
+        """
         if high == 0:
             high = len(self.logger_pdus)
         if high == low:
@@ -171,10 +204,21 @@ class PlaybackLoggerFileManager:
         else:
             return low
 
-    def _send(self, pdu: tuple[bytes, float]):
+    def _send(self, pdu: tuple[bytes, float]) -> None:
+        """
+        Puts a PDU into the pipe to be sent to the sender thread
+        :param pdu: tuple[bytes, float]     : A tuple of the pdu bytes, and its PacketTime
+        :return: None
+        """
         self.pdu_queue.send(pdu)
 
-    def load_logger(self, loggername: str):
+    def load_logger(self, loggername: str) -> None:
+        """
+        Loads a logger file from disk into memory of the program. Also translates it into a format that is useful to the
+        sender, so that PDUs may be sent.
+        :param loggername: str : Path to the logger file on disk
+        :return: None
+        """
         with lzma.open(f"{loggername}") as f:
             print(f"Reading loggerfile: {loggername}")
             raw_data = f.read().split(b"line_separator")[:-1]
@@ -198,7 +242,14 @@ class PlaybackLoggerFileManager:
             except ValueError:
                 print("seperator error")
 
-    def move_position_to_time(self, requested_time: float):
+    def move_position_to_time(self, requested_time: float) -> None:
+        """
+        Moves the playback position to the requested time.
+        Can be used during active playback.
+        If called during playback, playback is paused for the duration of the function
+        :param requested_time: float : Seconds since the start of the exercise (PacketTime)
+        :return: None
+        """
         need_to_unpause = False
         if not self.paused and not self._message_stop_playback:
             self.pause_playback()
@@ -211,18 +262,32 @@ class PlaybackLoggerFileManager:
         if need_to_unpause:
             self.unpause_playback()
 
-    def start_playback(self):
+    def start_playback(self) -> None:
+        """
+        Starts/unpauses the playback.
+        This is achieved through sending the PDU bytes, along with the PacketTime to the sender thread
+        This method pretty much solely manages the playback function, which runs in a separate thread, defined within.
+        :return: None
+        """
         if self.paused:
             self.unpause_playback()
             return
         self._message_stop_playback = False
 
         def playback():
+            """
+            Runs in a separate thread.
+            Iterates over the PDUs, from the current position, and sends the PDUs to the sender thread.
+            """
             pdus = self.logger_pdus[self.position_pointer:]
+            # Emptied on start of playback to ensure that entities from the future aren't displayed early.
             self.state_cache.clear()
 
             for n, pdu in enumerate(pdus):
                 if not self._message_stop_playback:
+                    # There is such a thing as too many messages, so the number sent is reduced by the
+                    # message_reduction_factor, and the playback speed, so as not to overwhelm the network connection
+                    # and cause further slowdowns.
                     if n % (self.playback_speed * self.message_reduction_factor) == 0 or self.playback_speed < 1:
                         if pdu[0][2] == 1:
                             # Removes the interpolation when paused by setting the velocity to (0,0,0)
@@ -256,17 +321,23 @@ class PlaybackLoggerFileManager:
                     self._messages_awaiting = []
                     return None
 
+        # Make sure that the pdu_queue has been emptied before beginning
         while self.pdu_queue.poll():
             self.pdu_queue.recv()
         self.starting_timestamp = datetime.datetime.now().timestamp()
         self.message_queue.send(
             ("starting_timestamp", self.starting_timestamp, self.logger_pdus[self.position_pointer][1]))
-        # Idiotproof check
+        # Idiotproof check, ensure the playback thread is not running right now
         if not self.playback_thread.is_alive():
             self.playback_thread = threading.Thread(target=playback, daemon=True)
             self.playback_thread.start()
 
-    def stop_playback(self, pause=False):
+    def stop_playback(self, pause=False) -> None:
+        """
+        Stops the playback by removing all entities, and sending a kill message to the playback thread.
+        :param pause: bool : Whether this is being called from the pause method.
+        :return: None
+        """
         self._message_stop_playback = True
 
         if not pause:
@@ -274,7 +345,11 @@ class PlaybackLoggerFileManager:
         if self.playback_thread.is_alive():
             self.message_queue.send(("stop",))
 
-    def remove_all_entities(self):
+    def remove_all_entities(self) -> None:
+        """
+        Iterates over all active entities, and makes them invisible so that they will no longer be seen in VRForces.
+        :return: None
+        """
         for e in self.state_cache:
             data = bytearray(self.state_cache[e])
             # appearance is 84-87 inclusive
@@ -288,11 +363,21 @@ class PlaybackLoggerFileManager:
 
             self._send((data, 0))
 
-    def _send_paused_locations(self):
+    def _send_paused_locations(self) -> None:
+        """
+        Send the data from the state cache, with a PacketTime of 0, so it is sent instantly.
+        :return: None
+        """
         for pdu in self.state_cache:
             self._send((self.state_cache[pdu], 0))
 
-    def pause_playback(self):
+    def pause_playback(self) -> None:
+        """
+        Pauses playback.
+        Written in similar style of start_playback(), just the function that is run in a separate thread sends the
+        PDUs with (0,0,0) in the velocity instead
+        :return: None
+        """
         self.stop_playback(pause=True)
         self.paused = True
 
@@ -308,7 +393,12 @@ class PlaybackLoggerFileManager:
             self.playback_thread = threading.Thread(target=paused_messages, daemon=True)
             self.playback_thread.start()
 
-    def unpause_playback(self):
+    def unpause_playback(self) -> None:
+        """
+        Restarts playback from paused.
+        Kinda deprecated since play() can also handle this
+        :return: None
+        """
         self.stop_playback()
         self.paused = False
         while self.playback_thread.is_alive():
@@ -318,11 +408,24 @@ class PlaybackLoggerFileManager:
         while not self.returning_information_queue.empty():
             self.returning_information_queue.get()
 
-    def set_playback_speed(self, playback_speed: float):
+    def set_playback_speed(self, playback_speed: float) -> None:
+        """
+        Sets the playback speed to the provided float.
+        Faster the 10x doesn't really go any faster, 10x is probably the limit, but it should be sufficient so I'm
+        not going to do anything about it right now.
+        :param playback_speed: float
+        :return: None
+        """
         self.playback_speed = playback_speed
         self.message_queue.send(("playback", playback_speed))
 
     def disable_sleep(self, should_sleep: bool):
+        """
+        Will set the sender thread to either deliver packets at the required time, or to just run as fast as it can
+        without sleeping.
+        :param should_sleep: bool : True means do sleep, False means no sleep
+        :return: None
+        """
         self.message_queue.send(("skip_sleep", should_sleep))
 
 
@@ -331,6 +434,7 @@ class PlaybackLoggerFile:
     This class provides easy control of the PlaybackLoggerFileManager
     It exists for connecting to the API, and for the GUI
     """
+
     def __init__(self, logger_name: str, exercise_id: int):
         """
         :param logger_name: str : absolute path to the lzma logger file that you want to load
@@ -420,7 +524,8 @@ if __name__ == "__main__":
     returning_information_queue = multiprocessing.SimpleQueue()
     playback = 1
 
-    plg = PlaybackLoggerFileManager("logs/exp_1_2102_2.lzma", pdu_sender, message_sender, returning_information_queue, 97)
+    plg = PlaybackLoggerFileManager("logs/exp_1_2102_2.lzma", pdu_sender, message_sender, returning_information_queue,
+                                    97)
     command = ""
     running_time = 0
     sender_process = multiprocessing.Process(target=sender,
