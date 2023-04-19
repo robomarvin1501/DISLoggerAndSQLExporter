0f5d317efdb711c4b185f968656d7c4de9f241ab
8Oldr <8Oldr@protonmail.com>
2023-04-19 12:58:07 +0300

    Documentation for logger_playback_api.py, and added the ability to set the exercise id through the API

--- logger_playback_api.py
+++ logger_playback_api.py
@@ -1,8 +1,12 @@
+"""
+This is an API for the PlaybackLoggerFile class. This is so that a GUI can be created in other languages that
+are incompatible with python (eg C#, Unity, etc).
+"""
 import re
 import socket
+import sys
 
 from logger_jumping import PlaybackLoggerFile
-
 
 # region udpsockets
 local_ip = "127.0.0.1"
@@ -14,7 +18,7 @@ udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 udp_server_socket.bind((local_ip, local_port))
 # endregion udpsockets
 
-plg = PlaybackLoggerFile("exp_1_2102_2.lzma", 97)
+plg = PlaybackLoggerFile("exp_1_2102_2.lzma", int(sys.argv[1]))
 
 while True:
     bytes_address_pair = udp_server_socket.recvfrom(buffersize)
@@ -32,6 +36,9 @@ while True:
     elif message[:8] == "playback" and len(ms := message.split(' ')) == 2 and re.match(
             r"^(?=.)([+-]?([0-9]*)(\.([0-9]+))?)$", ms[1]) is not None:
         plg.set_playback_speed(float(ms[1]))
+    elif message[:15] == "set_exercise_id" and len(ms := message.split(' ')) == 2 and re.match(r"^[0-9]*$",
+                                                                                               ms[1]) is not None:
+        plg.set_exercise_id(int(ms[1]))
     elif message == "show":
         print(plg.status())
 
